cbbada53ef450d934942e6bb17b34b87b647140c
ryan-heslin <rh3788@american.edu>
2022-09-11 12:23:08 -0400

    Day 20

--- solutions/day20.py
+++ solutions/day20.py
@@ -1,42 +1,46 @@
-target = 34000000
+number = 34000000
+part1_target = 34000000 // 10
+from math import floor
+from math import sqrt
+from math import inf
+from math import ceil
 
 # while sum(multiples[number]) != target:
 # increment
 
-def find_number(target): 
-    current = 2
-    multiples = {2 : 2} 
-    # Known divisors of each number (not including number itself)
-    divisors = {}
-    while True: 
-        current +=1
-        multiples[current] = 1
-        divisors[current] = set()
-        total = 1
-        # number is a multiple of itself
-        candidates = set(multiples.keys())
-        while candidates: 
-            this = candidates.pop()
-            countdown = multiples[this]
-            # Decrement 1, wrapping around at 0
-            if countdown == 1:
-                divisors[current].add(this) # current is divisble by this number
-                total += this
-                # If divisible by this number, also divisible by all divisiors of this number 
-                if this in divisors.keys():
-                    second_order_divisors = divisors[this]
-                    total +=  sum(second_order_divisors)
-                    candidates.difference_update(second_order_divisors)
-                # Reset countdown
-                multiples[this] = this
-            else:
-                multiples[this] = countdown - 1 #Not multiple, so decrement
-        if total * 10 >= target: 
-            yield current
-        #print(multiples)
-        #print(divisors)
-        #print(total)
 
-gen = find_number(target)
+def sum_divisors(target, max_divisor=inf):
+
+    current = 3
+    total = 0
+
+    while True:
+        # Algorithm from https://www.math.uh.edu/~minru/web/divis2.html#:~:text=The%20most%20basic%20method%20for,of%20positive%20divisors%20for%20n.
+        # All numbers divisible by 1 and themselves
+        total = current + float(total <= max_divisor)
+        root = floor(sqrt(current))
+        # optimize num // 50
+        for divisor in range(2, min(root, max_divisor) + 1):
+            divided = current / divisor
+            # Only add if integer divisor; don't double-count square roots
+            if divided % 1 == 0:
+                total += divisor * float(divided <= max_divisor)  + divided * float(
+                    divisor != divided
+                )
+        #print(f"{current} sum: {total}")
+        if total >= target:
+            yield current
+        current += 1
+
+
+# Start with number / 2
+# for divisor in [2..number // 2] do
+
+gen = sum_divisors(part1_target)
 part1 = next(gen)
 print(part1)
+
+part2_target = ceil(number / 11)
+gen = sum_divisors(part2_target, 50)
+part2 = next(gen)
+print(part2)
