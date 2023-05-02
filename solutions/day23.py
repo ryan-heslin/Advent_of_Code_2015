af5433e977c03c4b1741908dde9dba7e9961c682
ryan-heslin <rwheslin@gmail.com>
2023-05-02 19:36:19 -0400

    Eliminate regex library

--- solutions/day23.py
+++ solutions/day23.py
@@ -39,18 +39,28 @@ with open("inputs/day23.txt") as f:
     raw_input = f.read().splitlines()
 
 parsed_instructions = [
-    re.sub(r"([a-z])(?=,)", r"'\1'",  re.sub(r"^([a-z]{3})\s([\-+a-z0-9]+)(,\s[\-+0-9]+)?$", r"\1(\2\3" + f", line={i})", line))
+    re.sub(
+        r"([a-z])(?=,)",
+        r"'\1'",
+        re.sub(
+            r"^([a-z]{3})\s([\-+a-z0-9]+)(,\s[\-+0-9]+)?$",
+            r"\1(\2\3" + f", line={i})",
+            line,
+        ),
+    )
     for i, line in enumerate(raw_input)
 ]
 
-code = [compile(source = instr, filename = "", mode = "eval") for instr in parsed_instructions]
+code = [
+    compile(source=instr, filename="", mode="eval") for instr in parsed_instructions
+]
 
 line = 0
 while 0 <= line < len(code):
     line = eval(code[line])
 
 part1 = registers["b"]
-print(part1)
+print(int(part1))
 
 registers["a"] = 1
 registers["b"] = 0
--- solutions/day8.py
+++ solutions/day8.py
@@ -1,33 +1,50 @@
-import regex
+import re
 
 with open("inputs/day8.txt") as f:
     raw_input = f.read().splitlines()
 
 pattern = r"([a-z])|(?:\\{2})|(\\\")|(\\x[a-f0-9]{2})"
 encoding_gains = {"letters": 0, "backslash": 2, "quote": 2, "hex": 1}
+parts = dict(
+    zip(
+        encoding_gains.keys(),
+        (r"[a-z]+", "(?:\\\\{2})", r"\\\"", r"(?:\\x[a-f0-9]{2})"),
+    )
+)
 
 
 def parse_string(string, pattern):
-    return len(string), len(regex.findall(pattern, string))
+    return len(string), len(re.findall(pattern, string))
 
-def count_encoding(string, pattern):
-    matched = regex.match(pattern, string)
+
+def count_encoding(string):
     # Add 4 to escape both delimiting "" and add literal delimiting " to encoded string
     # Regular letters get no extras
     out = 4
-    if matched is not None:
-        out += sum(
-            encoding_gains[group] * len(captures)
-            for group, captures in matched.capturesdict().items()
-        )
+    for mtype, patt in parts.items():
+        # Remove backslashes to avoid false-positive hex escape matches
+        if mtype == "hex":
+            new = None
+            while new != string:
+                new = re.sub(r"(?:\\\\)+", "", string)
+                string = new
+        out += len(re.findall(patt, string[1:-1:])) * encoding_gains[mtype]
+
+    # if matched is not None:
+    #     out += sum(
+    #         encoding_gains[group] * len(captures)
+    #         for group, captures in matched.capturesdict().items()
+    #     )
+    # assert out == comp
     return out
+
 
 result = list(zip(*[parse_string(string, pattern) for string in raw_input]))
 part1 = sum(result[0]) - sum(result[1])
 print(part1)
 
 # Net difference in characters from escaping
-part2_pattern = r"^\"(?:(?P<letters>[a-z]+)|(?P<backslash>\\{2})|(?P<quote>\\\")|(?P<hex>\\x[a-f0-9]{2}))*\"$"
+# part2_pattern = r"^\"(?:(?P<letters>[a-z]+)|(?P<backslash>\\{2})|(?P<quote>\\\")|(?P<hex>\\x[a-f0-9]{2}))*\"$"
 
-part2 = sum(count_encoding(string, part2_pattern) for string in raw_input)
+part2 = sum(count_encoding(string) for string in raw_input)
 print(part2)
