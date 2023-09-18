7e790d716021eed417a6c10d6f951ad6d241f6d5
ryan-heslin <rwheslin@gmail.com>
2023-09-18 08:20:54 -0400

    Improve day 12

--- solutions/day12.py
+++ solutions/day12.py
@@ -1,19 +1,17 @@
-import re
 import json
+import re
 from collections import deque
 
 with open("inputs/day12.txt") as f:
     raw_input = f.read()
 
-numbers = [int(x.group(0)) for x in re.finditer(r"-?\d+",   raw_input)]
+numbers = list(map(int, (x.group(0) for x in re.finditer(r"-?\d+", raw_input))))
 part1 = sum(numbers)
 print(part1)
 
 queue = json.loads(raw_input)
-if type(queue) != list:
-    queue = [queue]
 
-queue = deque(queue)
+queue = deque([queue])
 
 total = 0
 while queue:
@@ -25,15 +23,15 @@ while queue:
         is_dict = True
     # Ignore current and all children
     additions = []
-    for el in current: 
+    for el in current:
         type_of = type(el)
         if type_of == int:
             this_sum += el
-        elif type_of in (dict, list): 
+        elif type_of in (dict, list):
             additions.append(el)
-        elif el == "red" and is_dict: 
+        elif el == "red" and is_dict:
             # Forget everything if red found
-            this_sum = 0 
+            this_sum = 0
             additions = []
             break
     total += this_sum
